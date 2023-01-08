310b3d46204b6294291e37c1b215ca7d5a37b2a0
Nikhil Kathait <nknick97@gmail.com>
2023-01-08 17:08:26 +0530

    Add project files.

---
+++ NikhilBank.Presentation/NikhilBank.cs
@@ -0,0 +1,34 @@
+﻿class Bank
+{   // Basic bank program without linking to SQL database
+    // Applicatin execution starts
+    static void Main()
+    {
+        // Display Title
+        System.Console.WriteLine("************NikhilBank************");
+        System.Console.WriteLine("::Login Page::");
+
+        // Declare variables to store username and password
+        string username = null, password = null;
+
+        // Read username from keyboard
+        System.Console.Write("Username: ");
+        username = System.Console.ReadLine();
+
+        if(username != "")
+        {
+            // Read password from keyboard
+            System.Console.Write("Password: ");
+            password = System.Console.ReadLine();
+        }
+
+        // Check username and password
+        if(username == "system" && password == "admin")
+        {
+            System.Console.WriteLine("TO DO: Main menu here");
+
+        }
+        // About to exit
+        System.Console.WriteLine("Thank You! Visit Again.");
+        System.Console.ReadKey();
+    }
+}
